b482ae8e200bfa0a93b748423dd3a63c081dd846
Louis Thevenet <louis.tvnt@gmail.com>
2024-05-29 20:12:58 +0200

    feat: configure network

--- modules/network.nix
+++ modules/network.nix
@@ -7,27 +7,20 @@
   config = {
     environment = {
       etc = {
-        "foo.conf".text = ''
-          launch_the_rockets = true
-        '';
-      };
-      systemPackages = [
-        pkgs.hello
-      ];
-    };
+        "network/interfaces".text = ''
+          auto lo
+          iface lo inet loopback
 
-    systemd.services = {
-      foo = {
-        enable = true;
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-        };
-        wantedBy = ["system-manager.target"];
-        script = ''
-          ${lib.getBin pkgs.hello}/bin/hello
-          echo "We launched the rockets!"
+
+          ifup enp1s0
+          allow-hotplug enp1s0
+          iface enp1s0 inet static
+          address 192.168.122.200/24
+          netmask 255.255.255.0
+          gateway 192.168.0.1
+          broadcast 192.168.0.255
         '';
+        #iface enp1s0 inet dhcp
       };
     };
   };
