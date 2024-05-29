4ef148c4336aad9f057503bac0fe8db2ee8fb4b1
Louis Thevenet <louis.tvnt@gmail.com>
2024-05-29 20:25:42 +0200

    feat: enable dhcp

--- modules/network.nix
+++ modules/network.nix
@@ -11,16 +11,10 @@
           auto lo
           iface lo inet loopback
 
-
           ifup enp1s0
           allow-hotplug enp1s0
-          iface enp1s0 inet static
-          address 192.168.122.200/24
-          netmask 255.255.255.0
-          gateway 192.168.0.1
-          broadcast 192.168.0.255
+          iface enp1s0 inet dhcp
         '';
-        #iface enp1s0 inet dhcp
       };
     };
   };
